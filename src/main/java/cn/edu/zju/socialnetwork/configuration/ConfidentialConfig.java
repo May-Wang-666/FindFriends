33cac9ad58affcc147c18468e3037dc171bb44e1
Yansongsongsong <yansongsongsong@gmail.com>
2019-01-18 00:14:51 +0800

    Update ConfidentialConfig.java
    modify the ConfidentialConfig.java with Java Bean conifg; Make it possible when check the password in user controller

--- src/main/java/cn/edu/zju/socialnetwork/configuration/ConfidentialConfig.java
+++ src/main/java/cn/edu/zju/socialnetwork/configuration/ConfidentialConfig.java
@@ -1,13 +1,23 @@
 package cn.edu.zju.socialnetwork.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class ConfidentialConfig implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new ConfidentialInterceptor()).addPathPatterns("/**");
+public class ConfidentialConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false).maxAge(3600);
+
+            }
+        };
     }
 }
